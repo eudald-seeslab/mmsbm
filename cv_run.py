b0713e49bb761e26dcf7a8cccbfc27f9c43b8f4f
Eudald <eudald@correig.net>
2021-07-22 16:57:28 +0200

    fixes in cv_run

--- cv_run.py
+++ cv_run.py
@@ -53,6 +53,10 @@ if __name__ == "__main__":
         test_indices = [
             a[0] for a in temp.groupby("student", as_index=False).apply(get_one).values
         ]
+        # FIXME: for some reason, I get a zero here...
+        if 0 in test_indices:
+            test_indices.remove(0)
+
         train_indices = [a for a in df.index if a not in test_indices]
         leftover_indices = [a for a in leftover_indices if a not in test_indices]
         temp = df.iloc[leftover_indices, :]
@@ -60,8 +64,8 @@ if __name__ == "__main__":
         # Crate the train and test sets for each fold
         # FIXME: this is objectively sub-optimal, but otherwise I need to make
         #  a ton of changes.
-        train = df.iloc[train_indices, :].to_csv(os.path.join("data", TRAIN_NAME), header=False, index=False)
-        test = df.iloc[test_indices, :].to_csv(os.path.join("data", TEST_NAME), header=False, index=False)
+        train = df.iloc[train_indices, :].to_csv(os.path.join("data", TRAIN_NAME), index=False)
+        test = df.iloc[test_indices, :].to_csv(os.path.join("data", TEST_NAME), index=False)
 
         mmsbm = MMSBM(
             train_set=TRAIN_NAME,
