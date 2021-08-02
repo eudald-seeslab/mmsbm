a8a66aef3fd2dc5851ca150bd2035f4a1e509f13
eudald <eudald@correig.net>
2021-08-02 10:18:04 +0200

    create a setup.py file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="mmsbm",
+    version="0.0.1",
+    description="Compute Mixed Membership Stochastic Block Models.",
+    py_modules=["mmsbm"],
+    package_dir={"": "src"}
+)
