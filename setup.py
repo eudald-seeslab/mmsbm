37e1e9152ccff75555a90e36cfac190f04faa8fc
Eudald <eudald@correig.net>
2021-08-06 06:05:41 +0200

    update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="mmsbm",
-    version="0.1.1",
+    version="0.1.2",
     description="Compute Mixed Membership Stochastic Block Models.",
     py_modules=["mmsbm", "expectation_maximization", "data_handler", "helpers"],
     package_dir={"": "src"},
