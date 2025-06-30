b8564c5da84c7eecf9878a2a483786de44326063
eudald <eudald.correig@urv.cat>
2025-06-30 11:11:56 +0200

    upgrade version to 0.4.0

---
+++ CHANGELOG.md
@@ -0,0 +1,15 @@
+# Changelog
+
+## [0.4.0] - 2025-06-30
+
+### Added
+- Implemented numba backend
+
+### Changed
+- Major efficiency improvements in numpy
+
+### Fixed
+- Nothing
+
+## [0.3.3] - Previous version
+- ... 
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="mmsbm",
-    version="0.3.3",
+    version="0.4.0",
     description="Compute Mixed Membership Stochastic Block Models.",
     py_modules=["mmsbm", "expectation_maximization", "data_handler", "helpers"],
     package_dir={"": "src"},
