36e67b61c35e11afb1c103bf66d46c961c05fdb7
eudald <eudald.correig@innovamat.com>
2025-07-01 12:38:57 +0200

    update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="mmsbm",
-    version="0.4.1",
+    version="0.4.2",
     description="Compute Mixed Membership Stochastic Block Models.",
     py_modules=["mmsbm", "expectation_maximization", "data_handler", "helpers", "logger"],
     package_dir={"": "src"},
