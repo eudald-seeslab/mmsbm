44eec738e140d2cba725d738fbe72fd5ae3910de
eudald <eudald.correig@innovamat.com>
2025-12-11 13:55:21 +0100

    avoid logging duplicates

--- src/logger.py
+++ src/logger.py
@@ -1,15 +1,23 @@
 import logging
 
 def setup_logger(name, log_file="mmsbm.log", level=logging.INFO):
-    handler = logging.FileHandler(log_file)
-    handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s %(message)s'))
-
-    console_handler = logging.StreamHandler()
-    console_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s %(message)s'))
-
+    """Configure or refresh a named logger each time this is called."""
     logger = logging.getLogger(name)
     logger.setLevel(level)
-    logger.addHandler(handler)
+    # Clear existing handlers to avoid duplicates
+    logger.handlers.clear()
+
+    formatter = logging.Formatter("%(asctime)s %(levelname)s %(message)s")
+
+    file_handler = logging.FileHandler(log_file, encoding="utf-8")
+    file_handler.setLevel(level)
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(level)
+    console_handler.setFormatter(formatter)
     logger.addHandler(console_handler)
 
+    logger.propagate = False
     return logger
