f450777fafcb217f3f550075b37e0828accd0562
eudald <eudald@correig.net>
2025-12-14 10:15:06 +0100

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="mmsbm",
-    version="1.0.4",
+    version="1.0.5",
     description="Compute Mixed Membership Stochastic Block Models.",
     py_modules=["mmsbm", "expectation_maximization", "data_handler", "helpers", "backend",
                 "logger", "kernels_numpy", "kernels_numba", "kernels_cupy"],
